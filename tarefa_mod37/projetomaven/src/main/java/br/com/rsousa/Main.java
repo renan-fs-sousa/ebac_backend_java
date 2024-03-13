01226bd9390a7c73c040cc8372d272150cbc55c3
renan-fs-sousa <renan.fs.sousa@gmail.com>
2024-03-13 13:12:33 -0300

    inserindo tarefa mod37

---
+++ tarefa_mod37/projetomaven/src/main/java/br/com/rsousa/Main.java
@@ -0,0 +1,12 @@
+package br.com.rsousa;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String hello = "Hello world!";
+        System.out.println(StringUtils.upperCase(hello));
+    }
+
+}
